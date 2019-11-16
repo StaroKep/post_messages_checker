566e6977bd4c588082752cd04628b13b9d1cc286
Kirill Eremin <kirilleremin@yandex-team.ru>
2019-11-16 23:52:56 +0300

    Please use this html page

---
+++ scripts.js
@@ -0,0 +1,40 @@
+var iframe = document.getElementById('iframe');
+var input = document.getElementById('input');
+var button = document.getElementById('button');
+var postMessagesEl = document.getElementById('post-messages');
+var savedUrl = localStorage.getItem('PaymentWidgetFrameUrl');
+
+if (savedUrl) {
+    input.value = savedUrl;
+    iframe.setAttribute('src', savedUrl);
+}
+
+window.addEventListener("message", receiveMessage, false);
+
+input.addEventListener('keyup', function (e) {
+    if (e.code === 'Enter') {
+        e.preventDefault();
+        e.stopPropagation();
+        updateIframeUrl();
+    }
+});
+
+button.addEventListener('click', function (e) {
+
+    e.preventDefault();
+    e.stopPropagation();
+    updateIframeUrl();
+
+});
+
+function updateIframeUrl() {
+    var url = document.getElementById('input').value;
+    localStorage.setItem('PaymentWidgetFrameUrl', url);
+    iframe.setAttribute('src', url);
+}
+
+function receiveMessage(event) {
+    postMessagesHistory = postMessagesEl.innerHTML + '<br/>';
+    postMessagesEl.innerHTML = postMessagesHistory + event.data;
+    console.log(event);
+}
